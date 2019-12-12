756ab6ae574608c3e2e3e16c73d3d39f46a81959
Defne Tunçer <defnetuncer@hacettepe.edu.tr>
2019-12-12 22:07:12 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import pickle
 import librosa, librosa.display
@@ -8,43 +7,90 @@ import numpy as np
 import random
 import pandas as pd
 import matplotlib.pyplot as plt
+import uuid
 
 app=Flask(__name__)
-#app = Flask(static_folder='C:\\Some\\Directory')
+count = 0
+
+list = ['a', 'about', 'add', 'address', 'after', 'all', 'also', 'am', 'an', 
+		'and', 'any', 'are', 'as', 'at', 'available', 'b', 'back', 'be', 'because', 
+		'been', 'before', 'best', 'book', 'books', 'business', 'but', 'buy', 'by', 
+		'c', 'can', 'car', 'center', 'city', 'click', 'comments', 'community', 'company', 
+		'contact', 'copyright', 'could', 'd', 'data', 'date', 'day', 'days', 'de', 'design', 
+		'details', 'development', 'did', 'do', 'does', 'e', 'each', 'ebay', 'education', 
+		'email', 'f', 'find', 'first', 'for', 'free', 'from', 'full', 'games', 'general', 
+		'get', 'go', 'good', 'great', 'group', 'had', 'has', 'have', 'he', 'health', 'help', 
+		'her', 'here', 'high', 'his', 'home', 'hotel', 'hotels', 'how', 'i', 'if', 'in', 
+		'info', 'information', 'international', 'internet', 'into', 'is', 'it', 'item', 
+		'items', 'its', 'jan', 'january', 'just', 'know', 'last', 'life', 'like', 'line',
+		'links', 'list', 'local', 'm', 'made', 'mail', 'make', 'management', 'many', 'map',
+		'may', 'me', 'member', 'message', 'more', 'most', 'music', 'must', 'my', 'n', 'name', 
+		'national', 'need', 'new', 'news', 'next', 'no', 'not', 'now', 'number', 'of', 'off', 
+		'office', 'on', 'one', 'online', 'only', 'or', 'order', 'other', 'our', 'out', 'over',
+		'p', 'page', 'part', 'people', 'please', 'pm', 'policy', 'post', 'posted', 'price', 
+		'privacy', 'product', 'products', 'program', 'public', 'r', 're', 'read', 'real', 
+		'report', 'research', 'results', 'review', 'reviews', 'right', 'rights', 's', 'said',
+		'school', 'search', 'see', 'send', 'service', 'services', 'set', 'sex', 'she', 'should', 
+		'site', 'so', 'software', 'some', 'state', 'store', 'such', 'support', 'system', 't',
+		'take', 'terms', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'these', 
+		'they', 'this', 'those', 'through', 'time', 'to', 'top', 'travel', 'two', 'type', 
+		'under', 'united', 'university', 'up', 'us', 'use', 'used', 'user', 'using', 'very',
+		'video', 'view', 'was', 'way', 'we', 'web', 'well', 'were', 'what', 'when', 'where',
+		'which', 'who', 'will', 'with', 'work', 'world', 'would', 'x', 'year', 'years', 'you', 'your']
+
+random.shuffle(list)
 
 
 class SVM:
 
 	prediction = "none"
-	
 	def detectOnsets(self, y, sr):
+		x=0
 		times = []
 		o_env = librosa.onset.onset_strength(y, sr=sr, hop_length=64)
 		onset_frames = librosa.onset.onset_detect(onset_envelope=o_env, sr=sr, backtrack=True, hop_length=64)
 		onset_times = librosa.times_like(o_env, sr=sr, hop_length=64)
-		start = onset_frames[0]*64
-		times.append(onset_times[onset_frames][0])
+		start = onset_frames[x]*64
 		
+		# remove noise		
+		while max(y[start:start+960])<0.12:
+			x+=1
+			start = onset_frames[x]*64
+		
+		times.append(onset_times[onset_frames][x])
+		
+		x=0
 		o_env = librosa.onset.onset_strength(np.asfortranarray(y[::-1]), sr=sr, hop_length=64)
 		onset_frames = librosa.onset.onset_detect(onset_envelope=o_env, sr=sr, backtrack=True, hop_length=64)            
 		onset_times = librosa.times_like(o_env, sr=sr, hop_length=64)
-		end = len(y)-onset_frames[0]*64
-		times.append(librosa.core.get_duration(y,sr)-onset_times[onset_frames][0])
+		end = len(y)-onset_frames[x]*64
+
+		# remove noise
+		while max(y[end-1920:end])<0.08:
+			x+=1
+			end = len(y)-onset_frames[x]*64
+		
+		times.append(librosa.get_duration(y,sr)-onset_times[onset_frames][x])
+		
 		return start,end,times
 		
 	def calculateFeatures(self):
-		y, sr = librosa.load('recording.mp3', mono=True)
+		y, sr = librosa.load('recording.mp3', mono=True)		
 		start,end,onset_times = self.detectOnsets(y,sr)
 		
+		# plot
 		plt.figure(figsize=(15, 5))
 		librosa.display.waveplot(y, sr, alpha=0.8)
-
-		plt.vlines(onset_times, 0, 1, color='r', alpha=0.9,linestyle='--', label='Onsets')
+		plt.vlines(onset_times[0], 0, 1, color='r', alpha=0.9,linestyle='--', label='Start')
+		plt.vlines(onset_times[1], 0, 1, color='g', alpha=0.9,linestyle='--', label='End')
 		plt.legend()
-		plt.savefig('static\\plt.png')
 		
+		# generate unique plot id
+		self.pltid = str(uuid.uuid1())
+		plt.savefig('static\\plt'+self.pltid+'.png')
 		y=y[start:end]
 		
+		# calculate features
 		chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
 		rmse = librosa.feature.rms(y=y)
 		spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
@@ -85,35 +131,6 @@ class SVM:
 
 svm = SVM()
 
-list = ['a', 'about', 'add', 'address', 'after', 'all', 'also', 'am', 'an', 
-		'and', 'any', 'are', 'as', 'at', 'available', 'b', 'back', 'be', 'because', 
-		'been', 'before', 'best', 'book', 'books', 'business', 'but', 'buy', 'by', 
-		'c', 'can', 'car', 'center', 'city', 'click', 'comments', 'community', 'company', 
-		'contact', 'copyright', 'could', 'd', 'data', 'date', 'day', 'days', 'de', 'design', 
-		'details', 'development', 'did', 'do', 'does', 'e', 'each', 'ebay', 'education', 
-		'email', 'f', 'find', 'first', 'for', 'free', 'from', 'full', 'games', 'general', 
-		'get', 'go', 'good', 'great', 'group', 'had', 'has', 'have', 'he', 'health', 'help', 
-		'her', 'here', 'high', 'his', 'home', 'hotel', 'hotels', 'how', 'i', 'if', 'in', 
-		'info', 'information', 'international', 'internet', 'into', 'is', 'it', 'item', 
-		'items', 'its', 'jan', 'january', 'just', 'know', 'last', 'life', 'like', 'line',
-		'links', 'list', 'local', 'm', 'made', 'mail', 'make', 'management', 'many', 'map',
-		'may', 'me', 'member', 'message', 'more', 'most', 'music', 'must', 'my', 'n', 'name', 
-		'national', 'need', 'new', 'news', 'next', 'no', 'not', 'now', 'number', 'of', 'off', 
-		'office', 'on', 'one', 'online', 'only', 'or', 'order', 'other', 'our', 'out', 'over',
-		'p', 'page', 'part', 'people', 'please', 'pm', 'policy', 'post', 'posted', 'price', 
-		'privacy', 'product', 'products', 'program', 'public', 'r', 're', 'read', 'real', 
-		'report', 'research', 'results', 'review', 'reviews', 'right', 'rights', 's', 'said',
-		'school', 'search', 'see', 'send', 'service', 'services', 'set', 'sex', 'she', 'should', 
-		'site', 'so', 'software', 'some', 'state', 'store', 'such', 'support', 'system', 't',
-		'take', 'terms', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'these', 
-		'they', 'this', 'those', 'through', 'time', 'to', 'top', 'travel', 'two', 'type', 
-		'under', 'united', 'university', 'up', 'us', 'use', 'used', 'user', 'using', 'very',
-		'video', 'view', 'was', 'way', 'we', 'web', 'well', 'were', 'what', 'when', 'where',
-		'which', 'who', 'will', 'with', 'work', 'world', 'would', 'x', 'year', 'years', 'you', 'your']
-
-random.shuffle(list)
-count = 0
-
 
 @app.route('/', methods = ['GET','POST']) 
 def index():
@@ -123,10 +140,9 @@ def index():
 def predict():
 	if request.method == 'POST' and request.files['recording'].filename:
 		request.files['recording'].save(os.path.join('./', 'recording.mp3'))
-	svm.predict(list[count])
-	return jsonify(result=str(svm.prediction))
+		svm.predict(list[count])
+	return jsonify(result=str(svm.prediction), next=svm.pltid)
 	
-
 @app.route('/skip', methods = ['GET']) 
 def skip():
 	global count
@@ -137,99 +153,3 @@ def skip():
 if __name__ == '__main__':
 	app.run(debug = True)
 
-=======
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-import pickle
-import librosa
-import sklearn as skl
-import os
-import numpy as np
-import random
-
-app=Flask(__name__)
-
-
-class SVM:
-
-	prediction = "none"
-	def calculateFeatures(self):
-		y, sr = librosa.load('recording.mp3', mono=True)
-		chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
-		rmse = librosa.feature.rms(y=y)
-		spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-		spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
-		rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
-		zcr = librosa.feature.zero_crossing_rate(y)
-		mfcc = librosa.feature.mfcc(y=y, sr=sr)
-		features = [np.mean(chroma_stft), np.mean(rmse), np.mean(spec_cent), np.mean(spec_bw), np.mean(rolloff), np.mean(zcr)]    
-		for e in mfcc:
-			features.append(np.mean(e))
-		features=[features]
-		scaler = skl.preprocessing.StandardScaler(copy=False)
-		scaler.fit_transform(features)
-		return features
-	
-	def predict(self, word):
-		model = pickle.load(open('ml/'+word+'.pkl', 'rb'))
-		features = self.calculateFeatures()
-		self.prediction = (model.predict_proba(features)[0][1]*100)//1
-		# %43 native user :(
-		#print(model.predict(features))
-		#print(model.predict_log_proba(features))
-		#print(model.predict_proba(features))
-
-svm = SVM()
-
-list = ['a', 'about', 'add', 'address', 'after', 'all', 'also', 'am', 'an', 
-		'and', 'any', 'are', 'as', 'at', 'available', 'b', 'back', 'be', 'because', 
-		'been', 'before', 'best', 'book', 'books', 'business', 'but', 'buy', 'by', 
-		'c', 'can', 'car', 'center', 'city', 'click', 'comments', 'community', 'company', 
-		'contact', 'copyright', 'could', 'd', 'data', 'date', 'day', 'days', 'de', 'design', 
-		'details', 'development', 'did', 'do', 'does', 'e', 'each', 'ebay', 'education', 
-		'email', 'f', 'find', 'first', 'for', 'free', 'from', 'full', 'games', 'general', 
-		'get', 'go', 'good', 'great', 'group', 'had', 'has', 'have', 'he', 'health', 'help', 
-		'her', 'here', 'high', 'his', 'home', 'hotel', 'hotels', 'how', 'i', 'if', 'in', 
-		'info', 'information', 'international', 'internet', 'into', 'is', 'it', 'item', 
-		'items', 'its', 'jan', 'january', 'just', 'know', 'last', 'life', 'like', 'line',
-		'links', 'list', 'local', 'm', 'made', 'mail', 'make', 'management', 'many', 'map',
-		'may', 'me', 'member', 'message', 'more', 'most', 'music', 'must', 'my', 'n', 'name', 
-		'national', 'need', 'new', 'news', 'next', 'no', 'not', 'now', 'number', 'of', 'off', 
-		'office', 'on', 'one', 'online', 'only', 'or', 'order', 'other', 'our', 'out', 'over',
-		'p', 'page', 'part', 'people', 'please', 'pm', 'policy', 'post', 'posted', 'price', 
-		'privacy', 'product', 'products', 'program', 'public', 'r', 're', 'read', 'real', 
-		'report', 'research', 'results', 'review', 'reviews', 'right', 'rights', 's', 'said',
-		'school', 'search', 'see', 'send', 'service', 'services', 'set', 'sex', 'she', 'should', 
-		'site', 'so', 'software', 'some', 'state', 'store', 'such', 'support', 'system', 't',
-		'take', 'terms', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'these', 
-		'they', 'this', 'those', 'through', 'time', 'to', 'top', 'travel', 'two', 'type', 
-		'under', 'united', 'university', 'up', 'us', 'use', 'used', 'user', 'using', 'very',
-		'video', 'view', 'was', 'way', 'we', 'web', 'well', 'were', 'what', 'when', 'where',
-		'which', 'who', 'will', 'with', 'work', 'world', 'would', 'x', 'year', 'years', 'you', 'your']
-
-random.shuffle(list)
-count = 0
-
-
-@app.route('/', methods = ['GET','POST']) 
-def index():
-	return render_template('index.html', word=list[count])
-
-@app.route('/predict', methods = ['GET','POST'])
-def predict():
-	#if request.method == 'POST' and request.files['recording'].filename:
-		#request.files['recording'].save(os.path.join('./', 'recording.mp3'))
-	svm.predict(list[count])
-	return jsonify(result="%"+str(svm.prediction)+" native speaker")
-	
-
-@app.route('/skip', methods = ['GET']) 
-def skip():
-	global count
-	count+=1
-	return redirect(url_for('index'))
-
-
-if __name__ == '__main__':
-	app.run(debug = True)
-
->>>>>>> 12b7eac158d59d647928f343cf49e2f8e0d71132
